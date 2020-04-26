26e6b7e4931f7d50b797bc47232d472c4e313f11
habedi77 <hossein.abedi1998@gmail.com>
2020-04-26 12:00:26 +0430

    Fixed bindings

--- WcfDuplexService/Duplex/CalculatorService.svc.cs
+++ WcfDuplexService/Duplex/CalculatorService.svc.cs
@@ -9,6 +9,7 @@ namespace WcfDuplexService.Duplex
 {
 	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in code, svc and config file together.
 	// NOTE: In order to launch WCF Test Client for testing this service, please select CalculatorService.svc or CalculatorService.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
 	public class CalculatorService : ICalculatorDuplex
 	{
         double result = 0.0D;
