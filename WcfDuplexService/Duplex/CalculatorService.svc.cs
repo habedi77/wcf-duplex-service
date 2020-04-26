e6fd5ab6f3f08fc5f8fc827800ab7b94fc2b3d2b
habedi77 <hossein.abedi1998@gmail.com>
2020-04-26 11:37:40 +0430

    Add project files.

---
+++ WcfDuplexService/Duplex/CalculatorService.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfDuplexService.Duplex
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalculatorService" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select CalculatorService.svc or CalculatorService.svc.cs at the Solution Explorer and start debugging.
+	public class CalculatorService : ICalculatorDuplex
+	{
+        double result = 0.0D;
+        string equation;
+
+        public CalculatorService()
+        {
+            equation = result.ToString();
+        }
+
+        public void Clear()
+        {
+            Callback.Equation(equation + " = " + result.ToString());
+            equation = result.ToString();
+        }
+
+        public void AddTo(double n)
+        {
+            result += n;
+            equation += " + " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        public void SubtractFrom(double n)
+        {
+            result -= n;
+            equation += " - " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        public void MultiplyBy(double n)
+        {
+            result *= n;
+            equation += " * " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        public void DivideBy(double n)
+        {
+            result /= n;
+            equation += " / " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        ICalculatorDuplexCallback Callback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<ICalculatorDuplexCallback>();
+            }
+        }
+    }
+}
