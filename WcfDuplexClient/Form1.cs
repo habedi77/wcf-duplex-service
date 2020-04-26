8fa5cfd216510f5b501a912c918a4eaaf3c55442
habedi77 <hossein.abedi1998@gmail.com>
2020-04-26 12:28:18 +0430

    Fixed callback handler (interface)

--- WcfDuplexClient/Form1.cs
+++ WcfDuplexClient/Form1.cs
@@ -14,20 +14,64 @@ namespace WcfDuplexClient
 {
 	public partial class Form1 : Form
 	{
+
+        private InstanceContext instanceContext;
+        private CalculatorDuplexClient client;
 		public Form1()
 		{
 			InitializeComponent();
 
 			// Construct InstanceContext to handle messages on callback interface
-			InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+			instanceContext = new InstanceContext(new CallbackHandler());
 
 			// Create a client
-			CalculatorDuplexClient client = new CalculatorDuplexClient(instanceContext);
+			client = new CalculatorDuplexClient(instanceContext);
 		}
 
 		private void buttonStart_Click(object sender, EventArgs e)
 		{
+            try
+            {
+                // Call the AddTo service operation.
+                double value = 100.00D;
+                client.AddTo(value);
 
-		}
+                // Call the SubtractFrom service operation.
+                value = 50.00D;
+                client.SubtractFrom(value);
+
+                // Call the MultiplyBy service operation.
+                value = 17.65D;
+                client.MultiplyBy(value);
+
+                // Call the DivideBy service operation.
+                value = 2.00D;
+                client.DivideBy(value);
+
+                // Complete equation.
+                client.Clear();
+
+                // Wait for callback messages to complete before
+                // closing.
+                System.Threading.Thread.Sleep(5000);
+
+                // Close the WCF client.
+                client.Close();
+                Console.WriteLine("Done!");
+            }
+            catch (TimeoutException timeProblem)
+            {
+                Console.WriteLine("The service operation timed out. " + timeProblem.Message);
+                client.Abort();
+                Console.Read();
+            }
+            catch (CommunicationException commProblem)
+            {
+                Console.WriteLine("There was a communication problem. " + commProblem.Message);
+                client.Abort();
+                Console.Read();
+            }
+
+        }
 	}
 }
