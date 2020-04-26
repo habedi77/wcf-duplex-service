43319202f4fafe8cf90d90e9f64a6e741bbd38c8
habedi77 <hossein.abedi1998@gmail.com>
2020-04-26 12:21:04 +0430

    Added callback contract Updated form

--- WcfDuplexClient/Form1.Designer.cs
+++ WcfDuplexClient/Form1.Designer.cs
@@ -28,13 +28,34 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Location = new System.Drawing.Point(514, 239);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(75, 23);
+			this.buttonStart.TabIndex = 0;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Controls.Add(this.buttonStart);
+			this.Name = "Form1";
+			this.Text = "Start";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button buttonStart;
 	}
 }
 
--- WcfDuplexClient/Form1.cs
+++ WcfDuplexClient/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WcfDuplexClient.CalculatorDuplexService;
 
 namespace WcfDuplexClient
 {
@@ -15,6 +17,17 @@ namespace WcfDuplexClient
 		public Form1()
 		{
 			InitializeComponent();
+
+			// Construct InstanceContext to handle messages on callback interface
+			InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+
+			// Create a client
+			CalculatorDuplexClient client = new CalculatorDuplexClient(instanceContext);
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
