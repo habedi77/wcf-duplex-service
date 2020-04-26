afee47423e98025cfef59a9651b380a61cf81881
habedi77 <hossein.abedi1998@gmail.com>
2020-04-26 13:09:03 +0430

    Added ChatContract

---
+++ WcfDuplexService/Chat/IChatDuplex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+
+namespace WcfDuplexService.Chat
+{
+	[ServiceContract(CallbackContract = typeof(IChatDuplex), SessionMode = SessionMode.Required)]
+	public interface IChatDuplex
+	{
+		[OperationContract(IsOneWay = true)]
+		void SendMessage(MessageDto message);
+
+		[OperationContract(IsOneWay = true)]
+		void SendUrgent(MessageDto message);
+	}
+}
---
+++ WcfDuplexService/Chat/MessageDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfDuplexService.Chat
+{
+	[DataContract]
+	public class MessageDto
+	{
+		[DataMember]
+		public string Message { get; set; }
+
+		[DataMember]
+		public DateTime SendDateTime { get; set; }
+	}
+}
